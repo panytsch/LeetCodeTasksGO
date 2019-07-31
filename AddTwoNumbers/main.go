03cc2b1c55f58443aff0e5f8c908fd379034544a
Panasiuk <gfyroman@gmail.com>
2019-08-01 02:03:46 +0300

    add two numbers still bad

--- AddTwoNumbers/main.go
+++ AddTwoNumbers/main.go
@@ -13,46 +13,23 @@ type ListNode struct {
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l1.Val == 0 && l2.Val == 0 {
 		return &ListNode{}
-	} else if l1.Val == 0 {
+	} else if l1.Val == 0 && l1.Next == nil {
 		return createNode(convSliceToInt(convIntToSlice(convertToNumber(l2))))
-	} else if l2.Val == 0 {
+	} else if l2.Val == 0 && l2.Next == nil {
 		return createNode(convSliceToInt(convIntToSlice(convertToNumber(l1))))
 	}
-	sum := rInt(convertToNumber(l1)) + rInt(convertToNumber(l2))
-	return createNode(reverseInt(convSliceToInt(convIntToSlice(sum))))
+	s1 := makeIntFromSlice(reverseString(strings.Split(getStringFromNode(l1), "")))
+	s2 := makeIntFromSlice(reverseString(strings.Split(getStringFromNode(l2), "")))
+	return createNode(reverseInt(convSliceToInt(convIntToSlice(s1 + s2))))
 }
 
-//func addTwoNodes(l1 *ListNode, l2 *ListNode) []int {
-//	var res []int
-//	more9 := false
-//	for {
-//		sum := 0
-//		if l1 == nil && l2 == nil {
-//			return res
-//		} else if l1 == nil {
-//			sum = l2.Val
-//		} else if l2 == nil {
-//			sum = l1.Val
-//		} else {
-//			sum = l1.Val + l2.Val
-//		}
-//
-//		if more9 == true {
-//			sum ++
-//			more9 = false
-//		}
-//
-//		if sum > 9 {
-//			sum = sum % 10
-//			more9 = true
-//		}
-//
-//		res = append(res, sum)
-//
-//		l1 = l1.Next
-//		l2 = l2.Next
-//	}
-//}
+func makeIntFromSlice(i []string) int {
+	s, e := strconv.Atoi(strings.Join(i, ""))
+	if e != nil {
+		return 0
+	}
+	return s
+}
 
 func convertToNumber(node *ListNode) int {
 	if node == nil {
@@ -92,24 +69,19 @@ func remove(s []int, index int) []int {
 	return append(s[:index], s[index+1:]...)
 }
 
-func rInt(x int) int {
-	if x > -1 && x < 10 {
+func reverseInt(x []int) []int {
+	l := len(x)
+	if l < 2 {
 		return x
 	}
-	sX := strings.Split(strconv.Itoa(x), "")
-	length := len(sX)
-	for i := length/2 - 1; i >= 0; i-- {
-		opp := length - 1 - i
-		sX[i], sX[opp] = sX[opp], sX[i]
+	for i := l/2 - 1; i >= 0; i-- {
+		opp := l - 1 - i
+		x[i], x[opp] = x[opp], x[i]
 	}
-	s, e := strconv.Atoi(strings.Join(sX, ""))
-	if e != nil {
-		return 0
-	}
-	return s
+	return x
 }
 
-func reverseInt(x []int) []int {
+func reverseString(x []string) []string {
 	l := len(x)
 	if l < 2 {
 		return x
